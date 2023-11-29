5b0424916bde09f89eb3b63a2f45cdbe263776b7
Arnie <arnie752@gmail.com>
2023-11-29 09:49:30 +0300

    Designed the Typing System
    Added a typing system that tracks the typed words in our word list

--- main.py
+++ main.py
@@ -9,12 +9,47 @@ canvas = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("My Board") 
 exit = False
 
+word_list = ['word', 'another', 'less', 'fruit', 'brick']
+selected_word = None
+typed_word = ''
+selected_word_index = 0
+
 # Main Game Loop
-while not exit: 
-	for event in pygame.event.get(): 
-		if event.type == pygame.QUIT: 
-			exit = True
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.key.key_code("a"):
-				print("A Key Pressed")
-	pygame.display.update() 
+while not exit:
+    keys = pygame.key.get_pressed()
+    for word in word_list:
+        if selected_word is None and keys[pygame.key.key_code(word[selected_word_index])]:
+            selected_word = word
+            typed_word += word[selected_word_index]
+            selected_word_index += 1
+            print("Selected Word: " + selected_word)
+            print("Typed Word: " + typed_word)
+            break
+        
+    if selected_word is not None:
+        if keys[pygame.key.key_code(selected_word[selected_word_index])] and len(selected_word) != selected_word_index:
+            typed_word += selected_word[selected_word_index]
+            selected_word_index += 1
+            print("Typed Word: " + typed_word)
+            
+        if typed_word == selected_word:
+            print("\'" + selected_word + "\' successfully typed" )
+            word_list.remove(selected_word)
+            selected_word_index = 0
+            typed_word = ''
+            selected_word = None
+
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+        pygame.display.update()
+    
+    
+	# for event in pygame.event.get(): 
+	# 	if event.type == pygame.QUIT: 
+	# 		exit = True
+	# 	if event.type == pygame.KEYDOWN:
+	# 		if event.key == pygame.key.key_code("a"):
+	# 			print("A Key Pressed")
+	# pygame.display.update() 

307288770c7c19a356aaee75101358d638e02991
Arnie <arnie752@gmail.com>
2023-11-29 10:56:37 +0300

    Created a Wave System which enhances the game loop
    Each wave becomes progressively harder by adding more words

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame 
-import modules.word_list_generator as generator
+import modules.word_list_generator as word_generator
 
 pygame.init() 
 
@@ -8,16 +8,29 @@ canvas = pygame.display.set_mode((500, 500))
 
 # TITLE OF CANVAS 
 pygame.display.set_caption("My Board") 
+
+# Used to maintain the Game Loop
 exit = False
 
-word_list = generator.generate_words(5)
-print(word_list)
+# Initialize the Wave System for Words
+wave = 1
+word_list = word_generator.generate_words(3 + wave)
+print("The words are: " + word_list.__str__())
+
 selected_word = None
 typed_word = ''
 selected_word_index = 0
 
 # Main Game Loop
 while not exit:
+    
+    # Update the word list if empty
+    if (len(word_list) == 0):
+        wave += 1
+        word_list = word_generator.generate_words(3 + wave)
+        print("The words are: " + word_list.__str__())
+    
+    # Read Keyboard input for Key Presses
     keys = pygame.key.get_pressed()
     for word in word_list:
         if selected_word is None and keys[pygame.key.key_code(word[selected_word_index])]:
